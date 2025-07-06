c730fbfe7e59ac165b69da942ef52a0699bf59b7
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-07-06 18:01:54 -0300

    add mysql relational migration

---
+++ database/migrations/relational/mysql_migration.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS mydb.Pessoa (
+  pk_pessoa VARCHAR(45) NOT NULL,
+  primaryName VARCHAR(45) NULL,
+  PRIMARY KEY (pk_pessoa));
+
+CREATE TABLE IF NOT EXISTS mydb.Nota (
+  pk_nota VARCHAR(45) NOT NULL,
+  averegeRating DECIMAL(23,4) NULL,
+  numVotes BIGINT(23) NULL,
+  PRIMARY KEY (pk_nota));
+
+CREATE TABLE IF NOT EXISTS mydb.Titulo (
+  pk_titulo VARCHAR(45) NOT NULL,
+  titleType VARCHAR(45) NULL,
+  primaryTitle VARCHAR(45) NULL,
+  starYear YEAR(4) NULL,
+  runtimeMinutes BIGINT(23) NULL,
+  genres VARCHAR(45) NULL,
+  Nota_pk_nota VARCHAR(45) NOT NULL,
+  PRIMARY KEY (pk_titulo, Nota_pk_nota),
+  INDEX fk_Titulo_Nota1_idx (Nota_pk_nota ASC) VISIBLE,
+  CONSTRAINT fk_Titulo_Nota1
+    FOREIGN KEY (Nota_pk_nota)
+    REFERENCES mydb.Nota (pk_nota)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS mydb.Papel (
+  category VARCHAR(45) NULL,
+  characters VARCHAR(45) NULL,
+  Pessoa_pk_pessoa VARCHAR(45) NOT NULL,
+  Titulo_pk_titulo VARCHAR(45) NOT NULL,
+  Titulo_Nota_pk_nota VARCHAR(45) NOT NULL,
+  PRIMARY KEY (Pessoa_pk_pessoa, Titulo_pk_titulo, Titulo_Nota_pk_nota),
+  INDEX fk_Papel_Titulo1_idx (Titulo_pk_titulo ASC, Titulo_Nota_pk_nota ASC) VISIBLE,
+  CONSTRAINT fk_Papel_Pessoa1
+    FOREIGN KEY (Pessoa_pk_pessoa)
+    REFERENCES mydb.Pessoa (pk_pessoa)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_Papel_Titulo1
+    FOREIGN KEY (Titulo_pk_titulo , Titulo_Nota_pk_nota)
+    REFERENCES mydb.Titulo (pk_titulo , Nota_pk_nota)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
