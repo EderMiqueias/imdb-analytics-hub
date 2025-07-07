718a2fd99cbca490fb0f5f655bc104cfba932db2
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-07-06 23:41:10 -0300

    add main file to start etl

---
+++ start_etl.py
@@ -0,0 +1,11 @@
+from etl import *
+from database import create_mysql_crud_service
+
+
+if __name__ == "__main__":
+    mysql_service = create_mysql_crud_service()
+    carregar_dim_titulo(mysql_service)
+    carregar_dim_pessoa(mysql_service)
+    carregar_dim_tempo(mysql_service)
+    carregar_dim_papel(mysql_service)
+    print("ETL concluído com sucesso.")
