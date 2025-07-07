3d4796dcf4790ab4c295cdc30db644144c3efa24
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-07-06 23:02:08 -0300

    fix sql migrations

--- database/migrations/dimensional/mysql_migration.sql
+++ database/migrations/dimensional/mysql_migration.sql
@@ -1,4 +1,4 @@
-CREATE TABLE DIM_Titulo (
+CREATE TABLE IF NOT EXISTS DIM_Titulo (
     pk_titulo INT PRIMARY KEY,
     titleType VARCHAR(45),
     primaryTitle VARCHAR(45),
@@ -6,23 +6,23 @@ CREATE TABLE DIM_Titulo (
     runtimeMinutes BIGINT(23)
 );
 
-CREATE TABLE DIM_Pessoa (
+CREATE TABLE IF NOT EXISTS DIM_Pessoa (
     pk_pessoa INT PRIMARY KEY,
     primaryName VARCHAR(45)
 );
 
-CREATE TABLE DIM_Tempo (
+CREATE TABLE IF NOT EXISTS DIM_Tempo (
     pk_tempo INT PRIMARY KEY,
     starYear YEAR(4)
 );
 
-CREATE TABLE DIM_Papel (
+CREATE TABLE IF NOT EXISTS DIM_Papel (
     pk_papel INT PRIMARY KEY,
     category VARCHAR(45),
     characters VARCHAR(45)
 );
 
-CREATE TABLE Fato_Avaliacao_Titulo (
+CREATE TABLE IF NOT EXISTS Fato_Avaliacao_Titulo (
     pk_fato INT PRIMARY KEY,
     DIM_Titulo_pk_titulo INT,
     DIM_Tempo_pk_tempo INT,
--- database/migrations/relational/mysql_migration.sql
+++ database/migrations/relational/mysql_migration.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS mydb.Pessoa (
+CREATE TABLE IF NOT EXISTS Pessoa (
   pk_pessoa VARCHAR(45) NOT NULL,
   primaryName VARCHAR(45) NULL,
   PRIMARY KEY (pk_pessoa));
 
-CREATE TABLE IF NOT EXISTS mydb.Nota (
+CREATE TABLE IF NOT EXISTS Nota (
   pk_nota VARCHAR(45) NOT NULL,
   averegeRating DECIMAL(23,4) NULL,
   numVotes BIGINT(23) NULL,
   PRIMARY KEY (pk_nota));
 
-CREATE TABLE IF NOT EXISTS mydb.Titulo (
+CREATE TABLE IF NOT EXISTS Titulo (
   pk_titulo VARCHAR(45) NOT NULL,
   titleType VARCHAR(45) NULL,
   primaryTitle VARCHAR(45) NULL,
@@ -21,11 +21,11 @@ CREATE TABLE IF NOT EXISTS mydb.Titulo (
   INDEX fk_Titulo_Nota1_idx (Nota_pk_nota ASC) VISIBLE,
   CONSTRAINT fk_Titulo_Nota1
     FOREIGN KEY (Nota_pk_nota)
-    REFERENCES mydb.Nota (pk_nota)
+    REFERENCES Nota (pk_nota)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-CREATE TABLE IF NOT EXISTS mydb.Papel (
+CREATE TABLE IF NOT EXISTS Papel (
   category VARCHAR(45) NULL,
   characters VARCHAR(45) NULL,
   Pessoa_pk_pessoa VARCHAR(45) NOT NULL,
@@ -35,11 +35,11 @@ CREATE TABLE IF NOT EXISTS mydb.Papel (
   INDEX fk_Papel_Titulo1_idx (Titulo_pk_titulo ASC, Titulo_Nota_pk_nota ASC) VISIBLE,
   CONSTRAINT fk_Papel_Pessoa1
     FOREIGN KEY (Pessoa_pk_pessoa)
-    REFERENCES mydb.Pessoa (pk_pessoa)
+    REFERENCES Pessoa (pk_pessoa)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_Papel_Titulo1
     FOREIGN KEY (Titulo_pk_titulo , Titulo_Nota_pk_nota)
-    REFERENCES mydb.Titulo (pk_titulo , Nota_pk_nota)
+    REFERENCES Titulo (pk_titulo , Nota_pk_nota)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
