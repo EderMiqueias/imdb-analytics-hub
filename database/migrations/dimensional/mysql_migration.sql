e32e27f385073558cc4bd60f91e3354f5f02bf0d
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-07-15 01:05:05 -0300

    fix: dimensional migrations varchar sizes

--- database/migrations/dimensional/mysql_migration.sql
+++ database/migrations/dimensional/mysql_migration.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS DIM_Titulo (
     pk_titulo INT PRIMARY KEY,
-    titleType VARCHAR(45),
-    primaryTitle VARCHAR(45),
-    genres VARCHAR(45),
+    titleType VARCHAR(255),
+    primaryTitle VARCHAR(255),
+    genres VARCHAR(255),
     runtimeMinutes BIGINT(23)
 );
 
 CREATE TABLE IF NOT EXISTS DIM_Pessoa (
     pk_pessoa INT PRIMARY KEY,
-    primaryName VARCHAR(45)
+    primaryName VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS DIM_Tempo (
@@ -17,21 +17,17 @@ CREATE TABLE IF NOT EXISTS DIM_Tempo (
 );
 
 CREATE TABLE IF NOT EXISTS DIM_Papel (
-    pk_papel INT PRIMARY KEY,
-    category VARCHAR(45),
-    characters VARCHAR(45)
+    pk_papel INT PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(255),
+    character_name VARCHAR(512)
 );
 
 CREATE TABLE IF NOT EXISTS Fato_Avaliacao_Titulo (
-    pk_fato INT PRIMARY KEY,
+    pk_fato INT PRIMARY KEY AUTO_INCREMENT,
     DIM_Titulo_pk_titulo INT,
     DIM_Tempo_pk_tempo INT,
-    DIM_Pessoa_pk_pessoa INT,
-    DIM_Papel_pk_papel INT,
     numVotes BIGINT(23),
     averageRating DECIMAL(4,2),
     FOREIGN KEY (DIM_Titulo_pk_titulo) REFERENCES DIM_Titulo(pk_titulo),
-    FOREIGN KEY (DIM_Tempo_pk_tempo) REFERENCES DIM_Tempo(pk_tempo),
-    FOREIGN KEY (DIM_Pessoa_pk_pessoa) REFERENCES DIM_Pessoa(pk_pessoa),
-    FOREIGN KEY (DIM_Papel_pk_papel) REFERENCES DIM_Papel(pk_papel)
+    FOREIGN KEY (DIM_Tempo_pk_tempo) REFERENCES DIM_Tempo(pk_tempo)
 );
