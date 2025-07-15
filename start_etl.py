3149492d041a1b008e92b817024524a4fc9eabe7
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-07-15 12:27:04 -0300

    abstrai executor dos etls separando por tabelas fato e dimensão

--- start_etl.py
+++ start_etl.py
@@ -2,12 +2,23 @@ from etl import *
 from database import create_mysql_crud_service
 
 
+def etl_dimensoes(service, dfs):
+    carregar_dim_titulo(service, dfs)
+    carregar_dim_pessoa(service, dfs)
+    carregar_dim_tempo(service, dfs)
+    carregar_dim_papel(service, dfs)
+
+
+def etl_fatos(service, dfs):
+    carregar_fato_avaliacao_titulo(service, dfs)
+    carregar_fato_pessoa(service, dfs)
+
+
 if __name__ == "__main__":
     mysql_service = create_mysql_crud_service()
-    dfs = carregar_dfs_from_tsvs()
+    loaded_dfs = carregar_dfs_from_tsvs()
 
-    carregar_dim_titulo(mysql_service, dfs)
-    carregar_dim_pessoa(mysql_service, dfs)
-    carregar_dim_tempo(mysql_service, dfs)
-    carregar_dim_papel(mysql_service, dfs)
+    etl_dimensoes(mysql_service, loaded_dfs)
+    etl_fatos(mysql_service, loaded_dfs)
+
     print("ETL concluído com sucesso.")
